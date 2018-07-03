dca6e60a455291576c5bdc78946e84be0f6f8982
zareth <zareth_abarca@hotmail.com>
2018-07-02 19:42:13 -0500

    Aquí se ven los datos de cada sede

--- src/data.js
+++ src/data.js
@@ -1,10 +1,11 @@
-// usar este archivo para todo el código que tenga que ver con mostrar los datos en la pantalla
-//let urlDirection obtiene el json de la data desde git hub 
 let urlDirection = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
 let fullSede = document.querySelector("#botonesSede");
-let btn = document.querySelector("#cdmx");
+let btn = document.querySelector("#lima");
+let btn1 = document.querySelector("#cdmx");
+let btn2 = document.querySelector("#santiago");
+let estudiantes = document.querySelector("#estudiantes");
 // usando el boton de las sedes llamamos a la función para que aparezca en consola data>lima>generacion>cuarta
-cdmx.addEventListener("click", function(){
+lima.addEventListener("click", function(){
     sede(); 
   })
 
@@ -12,15 +13,52 @@ cdmx.addEventListener("click", function(){
       fetch(urlDirection).then(function(datos){
       return datos.json();
       }).then(function(data){
-      console.log(data.lima);
+      //console.log(data.lima);
       let sede1 = data.lima.generacion;
-      console.log(Object.keys(sede1));
+      //console.log(Object.keys(sede1));
       let generaciones = Object.keys(sede1);
-      console.log(generaciones[0]); 
+      //console.log(generaciones[0]); 
     for(let i = 0; i<generaciones.length; i++) {
-        fullSede.innerHTML+= "<div>" + generaciones[i] + "</div>";
-    }
-      
+        fullSede.innerHTML+= "<div>" + generaciones[i] + "</div>";}
+        
       })
     }  
+    
 //Hasta aquí imprime las generaciones de lima :(  
+    cdmx.addEventListener("click", function(){
+        sede1(); 
+      }) 
+    let sede1 = function(){
+          fetch(urlDirection).then(function(datos){
+          return datos.json();
+          }).then(function(data){
+          console.log(data.mexico.generacion);
+          let sede1 = data.mexico.generacion;
+          console.log(Object.keys(sede1));
+          let generaciones = Object.keys(sede1);
+          
+          console.log(generaciones[0]); 
+        for(let i = 0; i<generaciones.length; i++) {
+            fullSede.innerHTML+= "<div>" + generaciones[i] + "</div>";
+            
+        }
+    })
+}
+santiago.addEventListener("click", function(){
+    sede2();
+  }) 
+let sede2 = function(){
+      fetch(urlDirection).then(function(datos){
+      return datos.json();
+      }).then(function(data){
+      console.log(data.santiago.generacion);
+      let sede2 = data.santiago.generacion;
+      console.log(Object.keys(sede2));
+      let generaciones = Object.keys(sede2);
+       
+      for(let i = 0; i<generaciones.length; i++) {
+        fullSede.innerHTML+= "<div>" + generaciones[i] + "</div>";
+        
+    }
+})
+}
