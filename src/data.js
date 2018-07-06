3d57e81a122ebe7ff9c198e1ac8d868e027ea21e
angyecastillo <lindacasags@hotmail.com>
2018-07-06 12:35:51 -0500

    ¨cambio¨

--- src/data.js
+++ src/data.js
@@ -1,11 +1,16 @@
+window.
+
+
+
+
 // usar este archivo para todo el código que tenga que ver con mostrar los datos en la pantalla
-//let urlDirection obtiene el json de la data desde git hub 
+//let urlDirection obtiene el json de la data desde git hub
 let urlDirection = "https://raw.githubusercontent.com/Laboratoria/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
 let fullSede = document.querySelector("#botonesSede");
 let btn = document.querySelector("#cdmx");
 // usando el boton de las sedes llamamos a la función para que aparezca en consola data>lima>generacion>cuarta
 cdmx.addEventListener("click", function(){
-    sede(); 
+    sede();
   })
 
   let sede = function(){
@@ -16,11 +21,11 @@ cdmx.addEventListener("click", function(){
       let sede1 = data.lima.generacion;
       console.log(Object.keys(sede1));
       let generaciones = Object.keys(sede1);
-      console.log(generaciones[0]); 
+      console.log(generaciones[0]);
     for(let i = 0; i<generaciones.length; i++) {
         fullSede.innerHTML+= "<div>" + generaciones[i] + "</div>";
     }
-      
+
       })
-    }  
-//Hasta aquí imprime las generaciones de lima :(  
+    }
+//Hasta aquí imprime las generaciones de lima :(
